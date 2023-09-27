accdfdcfdbd50943aee50e80578e3df1a93cfd94
DatFlowTS <info+github@datflow.de>
2023-09-27 10:40:12 +0200

    Rebuild. Only EL9 Support, yet

--- README.md
+++ README.md
@@ -1,38 +1,24 @@
-# Rocky Linux 8
+# Detecting your OS automatically and running the related script if supported:
 ```bash
-sh -c "$(curl -fsSL https://raw.github.com/datflowts/linuxinit/master/rockyinit)"
+sh -c "$(curl -fsSL https://raw.github.com/datflowts/linuxinit/master/setup.sh)"
 ```
 
-# CentOS 8.3++*
-```bash
-sh -c "$(curl -fsSL https://raw.github.com/datflowts/linuxinit/master/centosinit)"
-```
-
-# Oracle Linux 8
-```bash
-sh -c "$(curl -fsSL https://raw.github.com/datflowts/linuxinit/master/OL-Init)"
-```
-
-# Ubuntu 20.04
-```bash
-sh -c "$(curl -fsSL https://raw.github.com/datflowts/linuxinit/master/ubuntuinit)"
-```
-
-# Fedora 30*
-```bash
-sh -c "$(curl -fsSL https://raw.github.com/datflowts/linuxinit/master/fedorainit)"
-```
+## Currently supported OS:
+-   Enterprise Linux 9 (AlmaLinux, RockyLinux, RHEL, OEL, etc.)
+-   
  
  
   
-# MacOS [yeah, i know....] 10*
-```
-/bin/bash -c "$(curl -fsSL https://raw.github.com/datflowts/linuxinit/master/macosinit)"
-```
+
 
 
 
 # ATTENTION
-These scipts are made for myself, because I often have to deploy new servers and always do the same initial steps. That's because I've written the scripts. I'm lazy af. Remove `/root/.ssh/authorized_keys` | `/home/<username>/.ssh/authorized_keys` | `/etc/skel/.ssh/authorized_keys` after finishing this script, if they were installed. Also you should run `userdel -r default0`, if you didn't choose a username. The mentioned files are containing my public key from my ssh key pair and will give me access to every user without using a password. The user datflow is created within the zshinit scripts and is added to the wheel/sudo group. I don't think that I ever will know about it, when you use my scripts and so I won't even know about your server, but it's better, when you know about this. The scripts are primarily written for me and my purposes. Also you should be careful if you'll get asked, if you want to disable root login and password auth for SSH, if you don't have VNC or KVM console access. Previously created `authorized_keys` files will be removed in other steps of the script already.
+These scipts are made for myself, because I often have to deploy new servers and always do the same initial steps. That's because I've written the scripts. I'm lazy af. Remove `/root/.ssh/authorized_keys` | `/home/<username>/.ssh/authorized_keys` | `/etc/skel/.ssh/authorized_keys` after finishing this script, if they were installed. Also you should run `userdel -r default0`\*, if you didn't choose a username. The mentioned files are containing my public key from my ssh key pair and will give me access to every user without using a password. The user datflow is created within the zshinit scripts and is added to the wheel/sudo group. I don't think that I ever will know about it, when you use my scripts and so I won't even know about your server, but it's better, when you know about this. The scripts are primarily written for me and my purposes. Also you should be careful if you'll get asked, if you want to disable root login and password auth for SSH, if you don't have VNC or KVM console access. Previously created `authorized_keys` files will be removed in other steps of the script already.
 
-*\*Scripts marked weren't updated for a long time or won't be updated anymore in the future. Due to the EOL, CentOS will definitely not be supported nor used by me anymore. Instead I'll focus on Rocky Linux, Fedora, some Ubuntu and maybe in the future, AlmaLinux. MacOS will get an update, when I have to use it myself again.*
+# *BE CAREFUL!*
+*\*If you're not creating any user during this script and deleting 'default0', you'll be locked out by SSH! Root login will be disabled!*
+*\*If you didn't create a password for your user created by this script, you also won't be able to log in! 
+sshd was configured to use password + public key + TOTP as Multi-Faktor-Authentication. You need to set up a TOTP with google-authenticator
+and a password for your users. Also, a ssh-keypair is necessary. You'll always need all threee factors, once you created TOTP.
+Users without TOTP can still login with password + key*
---
+++ zsh/wheel.sh
@@ -0,0 +1,240 @@
+#!/bin/
+
+echo "Preparing to set up OhMyZSH...
+--------------------------
+" | tee -a $LOGFILE
+rm -rfv $HOME/.*ssh* $HOME/.*zsh* /etc/skel/.*ssh* /etc/skel/.*zsh* | tee -a $LOGFILE
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | tee -a $LOGFILE
+sed -i 's/RUNZSH=\${RUNZSH:-yes}/RUNZSH=\${RUNZSH:-no}/g' install.sh
+sed -i 's/RUNZSH=yes/RUNZSH=no/g' install.sh
+sed -i 's/CHSH=\${CHSH:-yes}/CHSH=\${CHSH:-no}/g' install.sh
+sed -i 's/CHSH=yes/CHSH=no/g' install.sh
+echo "
+--------------------------
+Running OhMyZSH installer:
+--------------------------
+--------------------------
+" | tee -a $LOGFILE
+sh install.sh | tee -a $LOGFILE
+rm -fv install.sh | tee -a $LOGFILE
+sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k/g' $HOME/.zshrc
+sed -i 's/\/root/\$HOME/g' $HOME/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/Pilaton/OhMyZsh-full-autoupdate.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/ohmyzsh-full-autoupdate
+git clone https://github.com/akash329d/zsh-alias-finder ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-alias-finder
+sed -i 's/^plugins=.*/plugins=\( \ngit z github ssh\-agent zsh\-alias\-finder \nohmyzsh\-full\-autoupdate zsh\-syntax\-highlighting \nzsh\-autosuggestions zsh\-history\-substring\-search \n\)/g' $HOME/.zshrc
+echo "
+--------------------------
+--------------------------
+Appending $HOME/.zshrc with the following content:
+--------------------------
+"  | tee -a $LOGFILE
+echo "
+
+############################################
+############################################
+
+# some customizations following here
+neofetch
+alias clear='clear;neofetch'
+alias _='sudo'
+alias su='su - '
+alias ll='ls -laAt'
+alias llt='ls -laARt'
+alias lld='ls -laAdRt'
+" | tee -a $HOME/.zshrc | tee -a $LOGFILE
+echo '
+# extending PATH environment
+export PATH="$HOME/sbin:$HOME/.local/sbin:$HOME/bin:$HOME/.local/bin:/usr/local/bin:/usr/local/sbin:$PATH"' | tee -a $HOME/.zshrc | tee -a $LOGFILE
+rm -rfv $HOME/.zshrc.pre-oh-my-zsh | tee -a $LOGFILE
+cp -afv $HOME/.*zsh* /etc/skel/ | tee -a $LOGFILE
+mkdir -v $HOME/.ssh | tee -a $LOGFILE;cd $HOME/.ssh
+curl https://github.com/datflowts.keys | tee -a authorized_keys | tee -a $LOGFILE
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC1rg+YI9Nwj1nAA0qyQROPeawTbZKYNc59igOaFdHr0sCfq1eXnC7GLXYmJhAu1N8g9xdqbUBtSwaNg54qR9NNH7S25Fuu4fRj6nuDky1Gws0GctvL//rTnKB3xS6MJ0FMn4s3Hg9q34LmQGwSDE1m76V115MyMRJIabzo1x1VOEgHZcxEIEI15hij2GpI5vOWYQrjrC4Y70/zQQmRNsbZDvCyrAvSOFLXi/Q74q+gY/7ic3V95ViFQdtt8pCORN3vX73/04kS3I8/EFALF3SO8MWQ8P9w8TT8Is1OjqaO09L7dpwyPtON5FucR7x4NMP/M3XBjWbvKZSkjLg4nzF9Bs7eqyRrqdMxNGcGOcfnqntY70OgHDRJpOloCT/f0RtUSI1ox8L7yRn5fpQt1830XM1tmWApf9jGlhUt9STBJi4m0p1OmMJmDzgkpunC28Q8wtIluaDQhd3fvtfjK0I4uMvbEzwgXEfldem0sAI0H8dIaXIuJ19a/2ILeYnw31s=" | tee -a authorized_keys | tee -a $LOGFILE
+cp -afv $HOME/.*ssh* /etc/skel/ | tee -a $LOGFILE
+cd /usr/local/bin
+echo "
+--------------------------
+--------------------------
+Setting up 'update' command:
+--------------------------
+" | tee -a $LOGFILE
+touch update
+echo '#\!/bin/zsh
+
+mkdir -p $HOME/scriptlogs/{init,update,config}
+UPDATE_LOG=$HOME/scriptslogs/update/$(date +%F).log
+
+# Optionally you can pass an argument for dnf, e.g. "--nobest"
+rm -rfv /usr/bin/neofetch | tee -a $UPDATE_LOG
+curl -s https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch -o /usr/bin/neofetch | tee -a $UPDATE_LOG
+chmod -v 555 /usr/bin/neofetch | tee -a $UPDATE_LOG 
+dnf -y upgrade --refresh $@ | tee -a $UPDATE_LOG
+dnf clean all | tee -a $UPDATE_LOG
+echo "
+Finished! Your system is up2date now! Reboot recommended.
+You can find details in $UPDATE_LOG.
+" | tee -a $UPDATE_LOG
+read -p "Reebot now? (Y/N, default N) => " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "You chosed to reboot after the update. 
+    Rebooting now..." | tee -a $LOGFILE
+    shutdown -r now | tee -a $UPDATE_LOG
+fi
+' | tee -a update | tee -a $LOGFILE
+echo "
+--------------------------
+--------------------------
+Yeah, 'update' command is now available!
+" | tee -a $LOGFILE
+cd ~
+echo "
+--------------------------
+--------------------------
+Installing powerline fonts
+--------------------------
+" | tee -a $LOGFILE
+git clone https://github.com/powerline/fonts.git --depth=1 | tee -a $LOGFILE
+cd fonts
+./install.sh | tee -a $LOGFILE
+cd ..
+rm -rf fonts
+cp -afv $HOME/.local /etc/skel/ | tee -a $LOGFILE
+chmod -v 555 /usr/local/bin/** | tee -a $LOGFILE
+touch .hushlogin
+touch /etc/skel/.hushlogin
+echo "
+#############################################################
+#############################################################
+#############################################################
+Operating as root could be risky. It's recommended to
+create other users with sudo privileges and disable root.
+So, let's define your default user! 
+Please, provide a username ('default' or 'cancel' to create
+a user with username 'default0'):
+"
+read USRNAME
+echo "
+--------------------------
+checking '$USRNAME' ...
+--------------------------
+" | tee -a $LOGFILE
+LOCK=0
+while [[ "$LOCK" -eq "0" ]]; do
+    if [[ "$USRNAME" =~ ^([0-9]{0,31}[[:lower:]]{1,32}[0-9]{0,31}){1,32}$ ]]; then
+        username_out="You've chosen a valid username. In the next step we create a user named '${USRNAME}'"
+        if [[ "$USRNAME" == "default" ]] | [[ "$USRNAME" == "cancel" ]]; then
+            USRNAME='default0'
+            username_out="You decided to not chose a name. I'll create a default user named '${USRNAME}'"
+        fi
+        echo $username_out | tee -a $LOGFILE
+        LOCK=1
+    else
+        echo "Please provide a valid unix username:
+- only 1-32 lowercase alphanumeric characters allowed
+- at least one alphabetic letter is required
+Try again or type 'cancel' or 'default' to cancel and proceed with setup
+Then a user named 'default0' will be created"
+        read USRNAME
+        echo "
+--------------------------
+checking '${USRNAME}' ...
+--------------------------
+" | tee -a $LOGFILE
+        LOCK=0
+    fi
+done
+export NEWUSER=$USRNAME
+useradd -G wheel $NEWUSER
+echo "
+--------------------------
+User '$NEWUSER' successfully created!
+--------------------------
+--------------------------
+" | tee -a $LOGFILE
+echo "Disabling root login via SSH and securing global authentication" | tee -a $LOGFILE
+mv /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+touch /etc/ssh/sshd_config
+echo "
+Include /etc/ssh/sshd_config.d/*.conf
+PermitRootLogin no
+PubkeyAuthentication yes
+AuthorizedKeysFile .ssh/authorized_keys
+PasswordAuthentication yes
+PermitEmptyPasswords no
+UsePAM yes
+Subsystem sftp  /usr/libexec/openssh/sftp-server
+" | tee -a /etc/ssh/sshd_config
+echo "
+--------------------------
+Creating SSH security policies for $NEWUSER...
+--------------------------
+" | tee -a $LOGFILE
+sshd_conf_file=/etc/ssh/sshd_config.d/$NEWUSER.conf
+touch $sshd_conf_file
+echo "
+Match User $NEWUSER
+    PasswordAuthentication no
+    PermitEmptyPasswords no
+    PubkeyAuthentication yes
+    KbdInteractiveAuthentication yes
+    ChallengeResponseAuthentication yes
+    AuthenticationMethods publickey,password publickey,keyboard-interactive
+" | tee -a $sshd_conf_file | tee -a $LOGFILE
+echo "
+# MFA
+auth       required     pam_google_authenticator.so nullock
+" | tee -a /etc/pam.d/sshd | tee -a /etc/pam.d/cockpit | tee -a $LOGFILE
+echo "
+#############################################################
+#############################################################
+#############################################################
+
+Security improved!
+-   run 'google-authenticator -t -d -f -r 3 -R 30 -w 3' as 
+    $NEWUSER to create TOTP Authentication.
+-   run 'passwd $NEWUSER' to create a password for
+    $NEWUSER (REQUIRED! Yo'll get locked out, if you don't!)
+-   for any new user you create, copy $sshd_conf_file with
+    the user's name and replace $NEWUSER inside the file 
+    with the new user's name.
+
+#############################################################
+#############################################################
+#############################################################
+
+Setting up pm2 daemon for $NEWUSER and for root.....
+" | tee -a $LOGFILE
+env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u $NEWUSER --hp /home/$NEWUSER | tee -a $LOGFILE
+pm2 startup | tee -a $LOGFILE
+echo "
+#############################################################
+#############################################################
+#############################################################
+
+Now we're checking your connection!
+
+#############################################################
+#############################################################
+#############################################################
+" | tee -a $LOGFILE
+speedtest --accept-license | tee -a $LOGFILE
+read -p "Install KDE Plasma Desktop, too? (Y/N, default N) => " -n 1 -r
+echo "" | tee -a $LOGFILE
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "Installing KDE Plasma Desktop...." | tee -a $LOGFILE
+    sh -c "$(curl -fsSL https://raw.github.com/datflowts/linuxinit/master/functions/kde_plasma_desktop.sh)"
+fi
+update '--best' | tee -a $LOGFILE
+echo '
+#############################################################
+#############################################################
+#############################################################
+Finished! Reboot recommended!
+' | tee -a $LOGFILE
+read -p "Reebot now? (Y/N, default N) => " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    shutdown -r now | tee -a $LOGFILE
+fi
