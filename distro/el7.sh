9b65f9d673713dbc212030d4c9a9d3d20d78a18c
DatFlow <info+github@datflow.de>
2023-10-09 17:14:29 +0200

    moved nodesource after key import

--- distro/el7.sh
+++ distro/el7.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 yum -y install wget curl dnf
-dnf -y install https://rpm.nodesource.com/pub_16.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm
-dnf -y install nodejs -y --setopt=nodesource-nodejs.module_hotfixes=1
 dnf -y install gettext make gcc-c++ make vim epel-release dnf-plugins-core cockpit | tee -a $LOGFILE
 cd /etc/pki/rpm-gpg || mkdir -p /etc/pki/rpm-gpg ; cd /etc/pki/rpm-gpg || exit 1
 wget https://rpm.nodesource.com/gpgkey/nodesource.gpg.key
@@ -12,6 +10,8 @@ rpm --import endpoint-rpmsign-7.pub
 rpm -qi gpg-pubkey-703df089 | gpg --with-fingerprint
 cd || exit 1
 dnf -y install https://packages.endpointdev.com/rhel/7/os/x86_64/endpoint-repo.x86_64.rpm
+dnf -y install https://rpm.nodesource.com/pub_16.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm
+dnf -y install nodejs -y --setopt=nodesource-nodejs.module_hotfixes=1
 sh -c "$(curl -fsSL https://raw.github.com/datflowts/linuxinit/master/functions/install_45drives_repo.sh)"
 dnf -y install cockpit-{packagekit,sosreport,storaged,networkmanager,selinux,kdump,navigator} | tee -a $LOGFILE
 systemctl enable --now cockpit.socket && systemctl start cockpit.socket | tee -a $LOGFILE
