9d7c01bec714720f95ffa053612152d46491faf9
DatFlowTS <info+github@datflow.de>
2023-10-09 17:18:43 +0200

    correct  installation order

--- distro/el7.sh
+++ distro/el7.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-
+{
 yum -y install wget curl dnf
-dnf -y install gettext make gcc-c++ make vim epel-release dnf-plugins-core cockpit | tee -a $LOGFILE
+dnf -y install gettext make gcc-c++ make vim epel-release dnf-plugins-core cockpit
+sh -c "$(curl -fsSL https://raw.github.com/datflowts/linuxinit/master/functions/install_45drives_repo.sh)"
+dnf -y install cockpit-{packagekit,sosreport,storaged,networkmanager,selinux,kdump,navigator}
+systemctl enable --now cockpit.socket && systemctl start cockpit.socket
+curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.rpm.sh | sudo bash
+dnf -y config-manager --set-enabled {powertools,epel,extras,plus}
+dnf -y upgrade --refresh 
 cd /etc/pki/rpm-gpg || mkdir -p /etc/pki/rpm-gpg ; cd /etc/pki/rpm-gpg || exit 1
 wget https://rpm.nodesource.com/gpgkey/nodesource.gpg.key
 wget https://packages.endpointdev.com/endpoint-rpmsign-7.pub
@@ -12,17 +18,12 @@ cd || exit 1
 dnf -y install https://packages.endpointdev.com/rhel/7/os/x86_64/endpoint-repo.x86_64.rpm
 dnf -y install https://rpm.nodesource.com/pub_16.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm
 dnf -y install nodejs -y --setopt=nodesource-nodejs.module_hotfixes=1
-sh -c "$(curl -fsSL https://raw.github.com/datflowts/linuxinit/master/functions/install_45drives_repo.sh)"
-dnf -y install cockpit-{packagekit,sosreport,storaged,networkmanager,selinux,kdump,navigator} | tee -a $LOGFILE
-systemctl enable --now cockpit.socket && systemctl start cockpit.socket | tee -a $LOGFILE
-curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.rpm.sh | sudo bash >> $LOGFILE
-dnf -y config-manager --set-enabled {powertools,epel,extras,plus} | tee -a $LOGFILE
-dnf -y upgrade --refresh  | tee -a $LOGFILE
-curl -s https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch -o /usr/bin/neofetch | tee -a $LOGFILE
-chmod -v 555 /usr/bin/neofetch | tee -a $LOGFILE
-dnf -y install git nodejs zsh speedtest google-authenticator | tee -a $LOGFILE
-npm i -g typescript pm2 | tee -a $LOGFILE
+curl -s https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch -o /usr/bin/neofetch
+chmod -v 555 /usr/bin/neofetch
+dnf -y install git nodejs zsh speedtest google-authenticator
+npm i -g typescript pm2
 sed -i 's/bash/zsh/g' /etc/default/useradd
 usermod --shell /bin/zsh root
 cd ~
 sh -c "$(curl -fsSL https://raw.github.com/datflowts/linuxinit/master/zsh/wheel.sh)"
+} | tee -a $LOGFILE
--- distro/el9.sh
+++ distro/el9.sh
@@ -2,18 +2,18 @@
 
 {
     yum -y install curl wget gcc-c++ make vim dnf epel-release dnf-plugins-core cockpit
-    cd /etc/pki/rpm-gpg || mkdir -p /etc/pki/rpm-gpg ; cd /etc/pki/rpm_gpg || exit 1
-    wget https://packages.endpointdev.com/endpoint-rpmsign-9.pub
-    rpm --import endpoint-rpmsign-9.pub
-    rpm -qi gpg-pubkey-4d996065 | gpg --show-keys --with-fingerprint
-    cd || exit 1
-    dnf -y install https://packages.endpointdev.com/rhel/9/main/x86_64/endpoint-repo.noarch.rpm
     dnf -y install --skip-broken cockpit-{packagekit,sosreport,storaged,networkmanager,selinux,kdump,navigator,podman,certificates}
     systemctl enable --now cockpit.socket && systemctl start cockpit.socket
     curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.rpm.sh
     dnf -y install https://pkgs.dyn.su/el9/base/x86_64/raven-release-1.0-4.el9.noarch.rpm
     dnf -y config-manager --set-enabled {raven,raven-extras,crb,epel,extras,plus}
     dnf -y upgrade --refresh
+    cd /etc/pki/rpm-gpg || mkdir -p /etc/pki/rpm-gpg ; cd /etc/pki/rpm_gpg || exit 1
+    wget https://packages.endpointdev.com/endpoint-rpmsign-9.pub
+    rpm --import endpoint-rpmsign-9.pub
+    rpm -qi gpg-pubkey-4d996065 | gpg --show-keys --with-fingerprint
+    cd || exit 1
+    dnf -y install https://packages.endpointdev.com/rhel/9/main/x86_64/endpoint-repo.noarch.rpm
     curl -sL https://rpm.nodesource.com/setup_current.x | sudo -E bash -
     curl -s https://raw.githubusercontent.com/dylanaraps/neofetch/master/neofetch -o /usr/bin/neofetch
     chmod -v 555 /usr/bin/neofetch
