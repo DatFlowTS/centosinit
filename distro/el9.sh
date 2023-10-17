27bc15518b800fb3920990bf6e82a6866531d15a
DatFlow <info+github@datflow.de>
2023-10-17 12:38:32 +0200

    rpm-gpg

--- distro/el9.sh
+++ distro/el9.sh
@@ -5,7 +5,7 @@
     dnf -y install gcc-c++ make vim epel-release dnf-plugins-core cockpit
     dnf -y install https://pkgs.dyn.su/el9/base/x86_64/raven-release-1.0-4.el9.noarch.rpm
     dnf -y config-manager --set-enabled {raven,raven-extras,crb,epel,extras,plus}
-    cd /etc/pki/rpm-gpg || mkdir -p /etc/pki/rpm_gpg ; cd /etc/pki/rpm_gpg || exit 1
+    cd /etc/pki/rpm-gpg || mkdir -p /etc/pki/rpm-gpg ; cd /etc/pki/rpm-gpg || exit 1
     wget https://rpm.nodesource.com/gpgkey/nodesource.gpg.key
     wget https://packages.endpointdev.com/endpoint-rpmsign-9.pub
     rpm --import /etc/pki/rpm-gpg/nodesource.gpg.key
