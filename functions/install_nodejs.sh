ffa0f4eff29fbd0aa3f425737b892627ccc96a80
DatFlowTS <info+github@datflow.de>
2024-04-25 19:44:33 +0200

    optimizations

--- functions/install_nodejs.sh
+++ functions/install_nodejs.sh
@@ -3,12 +3,13 @@
 LOG_FILE=$(sh -c "$(curl -fsSL https://raw.github.com/datflowts/linuxinit/master/functions/provide_logfile.sh)" 'setup')
 CONFIG_LOG=$(sh -c "$(curl -fsSL https://raw.github.com/datflowts/linuxinit/master/functions/provide_logfile.sh)" 'config')
 
+sh -c "$(curl -fsSL https://raw.github.com/datflowts/linuxinit/master/functions/create_bashrc.sh)"
 {
     # installs NVM (Node Version Manager)
     curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
     
     # shellcheck source=/dev/null
-    source "$HOME/.zshrc"
+    source "$HOME/.bashrc"
     
     # download and install latest available Node.js
     nvm install node
